ed00f80ef6e53335a78598e106505aa241fa87d0
Teja <starteja.007@gmail.com>
2022-07-09 11:53:22 +0530

    Add cam

--- public/index.js
+++ public/index.js
@@ -6,3 +6,28 @@ function handleFiles(event) {
 
 
 document.getElementById("upload").addEventListener("change", handleFiles, false);
+
+var stop = function () {
+  var stream = video.srcObject;
+  var tracks = stream.getTracks();
+  for (var i = 0; i < tracks.length; i++) {
+      var track = tracks[i];
+      track.stop();
+  }
+  video.srcObject = null;
+}
+var start = function () {
+  var video = document.getElementById('video'),
+      vendorUrl = window.URL || window.webkitURL;
+  if (navigator.mediaDevices.getUserMedia) {
+      navigator.mediaDevices.getUserMedia({ video: true })
+          .then(function (stream) {
+              video.srcObject = stream;
+          }).catch(function (error) {
+              console.log("Something went wrong!");
+          });
+  }
+}
+$(function () {
+  start();
+});  
