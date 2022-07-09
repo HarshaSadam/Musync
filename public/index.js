abbc73d79529d14aa9b7d8cd7d41c1ea180f5062
Teja <starteja.007@gmail.com>
2022-07-09 12:10:16 +0530

    Add synchroniser for audio and cam

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
+var aud=document.getElementById("audio");
 function handleFiles(event) {
   var files = event.target.files;
   $("#src").attr("src", URL.createObjectURL(files[0]));
-  document.getElementById("audio").load();
+  aud.load();
+  
 }
 
 
@@ -14,6 +16,7 @@ var stop = function () {
       var track = tracks[i];
       track.stop();
   }
+  aud.pause();
   video.srcObject = null;
 }
 var start = function () {
@@ -23,6 +26,7 @@ var start = function () {
       navigator.mediaDevices.getUserMedia({ video: true })
           .then(function (stream) {
               video.srcObject = stream;
+              aud.play();
           }).catch(function (error) {
               console.log("Something went wrong!");
           });
