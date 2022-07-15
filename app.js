33d2bd16c267b98c73a28ea2cb2ca6cecfedc86f
Harsha <sadamharshavardhan@gmail.com>
2022-07-15 11:36:57 +0530

    Add zooming functionality

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+const cors=require("cors");
+const multer = require('multer')
 const express=require('express');
-const app=express()
-app.use(express.static('public'))
+const app=express();
+app.use(express.static('public'));
+app.use(cors());
+const port =process.env.PORT || 3000;
 
 
 app.get("/",(req,res)=>{
-  res.sendFile(__dirname+'index.html');
+  res.sendFile(__dirname+'/public/audbackend.html');
 });
 
-app.listen(process.env.PORT || 3000,()=>{
-  console.log("Listening");
+
+app.listen(port,()=>{
+  console.log("Listening at",port);
 });
+
+
